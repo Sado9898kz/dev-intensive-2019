9097f1da307512f2c3fcf0361d01d1b85ab1c6e0
sado98 <sado9898kz@gmail.com>
2020-03-18 17:06:24 +0600

    create pattern: Builder

--- app/src/main/java/ru/skillbranch/devintensive/models/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -4,14 +4,14 @@ import ru.skillbranch.devintensive.utils.Utils
 import java.util.*
 
 data class User(
-        val id: String,
-        var firstName: String?,
-        var lastName: String?,
-        var avatar: String?,
-        var rating: Int = 0,
-        var respect: Int = 0,
-        val lastVisit: Date? = Date(),
-        val isOnline: Boolean = false
+    val id: String,
+    var firstName: String?,
+    var lastName: String?,
+    var avatar: String?,
+    var rating: Int = 0,
+    var respect: Int = 0,
+    val lastVisit: Date? = Date(),
+    val isOnline: Boolean = false
 ) {
     constructor(id: String, firstName: String?, lastName: String?)
             : this(id = id, firstName = firstName, lastName = lastName, avatar = null)
@@ -19,8 +19,10 @@ data class User(
     constructor(id: String) : this(id, firstName = "John", lastName = "Doe")
 
     init {
-        println("It`s Alive!!! \n" +
-                "${if (lastName === "Doe") "His name is $firstName $lastName" else "And his name is $firstName $lastName"}\n")
+        println(
+            "It`s Alive!!! \n" +
+                    "${if (lastName === "Doe") "His name is $firstName $lastName" else "And his name is $firstName $lastName"}\n"
+        )
     }
 
     companion object Factory {
@@ -31,5 +33,25 @@ data class User(
             val (firstName, lastName) = Utils.parseFullName(fullName)
             return User(id = "$lastId", firstName = firstName, lastName = lastName)
         }
+
+        fun Builder(
+            id: String = "$lastId",
+            firstName: String?,
+            lastName: String?,
+            avatar: String?,
+            rating: Int = 0,
+            respect: Int = 0,
+            lastVisit: Date? = Date(),
+            isOnline: Boolean = false
+        ) = User(
+            id = id,
+            firstName = firstName,
+            lastName = lastName,
+            avatar = avatar,
+            rating = rating,
+            respect = respect,
+            lastVisit = lastVisit,
+            isOnline = isOnline
+        )
     }
 }
